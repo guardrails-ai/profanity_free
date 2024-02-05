a1f619c5733c2e48a1190582943d6a0338344ed1
Safeer Mohiuddin <safeer@guardrailsai.com>
2024-02-05 08:15:23 +0000

    uploading profanity_free validator

--- test/test-validator.py
+++ test/test-validator.py
@@ -2,13 +2,13 @@
 # pytest test/test-validator.py
 
 from guardrails import Guard
-from validator import RegexMatch
+from validator import ProfanityFree
 
 # We use 'refrain' as the validator's fail action,
 #  so we expect failures to always result in a guarded output of None
 # Learn more about corrective actions here:
 #  https://www.guardrailsai.com/docs/concepts/output/#%EF%B8%8F-specifying-corrective-actions
-guard = Guard.from_string(validators=[RegexMatch(regex="a.*", match_type="fullmatch", on_fail="refrain")])
+guard = Guard.from_string(validators=[ProfanityFree(on_fail="refrain")])
 
 def test_pass():
   test_output = "a test value"
@@ -16,6 +16,6 @@ def test_pass():
   assert(guarded_output is test_output)
 
 def test_fail():
-  test_output = "b test value"
+  test_output = "you suck"
   raw_output, guarded_output, *rest = guard.parse(test_output)
   assert(guarded_output is None)
--- validator/main.py
+++ validator/main.py
@@ -1,8 +1,4 @@
-import re
-import string
-from typing import Any, Callable, Dict, Optional
-
-import rstr
+from typing import Any, Dict
 
 from guardrails.validator_base import (
     FailResult,
@@ -13,59 +9,36 @@ from guardrails.validator_base import (
 )
 
 
-@register_validator(name="guardrails/regex_match", data_type="string")
-class RegexMatch(Validator):
-    """Validates that a value matches a regular expression.
+@register_validator(name="guardrails/profanity_free", data_type="string")
+class ProfanityFree(Validator):
+    """Validates that a translated text does not contain profanity language.
+
+    This validator uses the `alt-profanity-check` package to check if a string
+    contains profanity language.
 
     **Key Properties**
 
     | Property                      | Description                       |
     | ----------------------------- | --------------------------------- |
-    | Name for `format` attribute   | `regex_match`                     |
+    | Name for `format` attribute   | `is-profanity-free`               |
     | Supported data types          | `string`                          |
-    | Programmatic fix              | Generate a string that matches the regular expression |
-
-    Args:
-        regex: Str regex pattern
-        match_type: Str in {"search", "fullmatch"} for a regex search or full-match option
-    """  # noqa
-
-    def __init__(
-        self,
-        regex: str,
-        match_type: Optional[str] = None,
-        on_fail: Optional[Callable] = None,
-    ):
-        # todo -> something forces this to be passed as kwargs and therefore xml-ized.
-        # match_types = ["fullmatch", "search"]
-
-        if match_type is None:
-            match_type = "fullmatch"
-        assert match_type in [
-            "fullmatch",
-            "search",
-        ], 'match_type must be in ["fullmatch", "search"]'
-
-        super().__init__(on_fail=on_fail, match_type=match_type, regex=regex)
-        self._regex = regex
-        self._match_type = match_type
+    | Programmatic fix              | None                              |
+    """
 
     def validate(self, value: Any, metadata: Dict) -> ValidationResult:
-        p = re.compile(self._regex)
-        """Validates that value matches the provided regular expression."""
-        # Pad matching string on either side for fix
-        # example if we are performing a regex search
-        str_padding = (
-            "" if self._match_type == "fullmatch" else rstr.rstr(string.ascii_lowercase)
-        )
-        self._fix_str = str_padding + rstr.xeger(self._regex) + str_padding
+        try:
+            from profanity_check import predict  # type: ignore
+        except ImportError:
+            raise ImportError(
+                "`is-profanity-free` validator requires the `alt-profanity-check`"
+                "package. Please install it with `poetry add profanity-check`."
+            )
 
-        if not getattr(p, self._match_type)(value):
+        prediction = predict([value])
+        if prediction[0] == 1:
             return FailResult(
-                error_message=f"Result must match {self._regex}",
-                fix_value=self._fix_str,
+                error_message=f"{value} contains profanity. "
+                f"Please return a profanity-free output.",
+                fix_value="",
             )
         return PassResult()
-
-    def to_prompt(self, with_keywords: bool = True) -> str:
-        return "results should match " + self._regex
